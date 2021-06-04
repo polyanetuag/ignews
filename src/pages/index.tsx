0463dd30b01cd0618fbe3a48c4775fad45333918
Polyane Tuag <pollyportugues@hotmail.com>
2021-06-04 19:36:40 -0300

    Definindo estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head'
 
-import styles from '../styles/home.module.scss'
-
 export default function Home() {
   return (
     <>
@@ -9,7 +7,7 @@ export default function Home() {
           <title>ig.News</title>
         </Head>
 
-      <h1 className={styles.title}>
+      <h1>
         Hello world
       </h1>
     </>
