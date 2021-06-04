b74f261546fd8d7c38e4b80627bc1cda5e0af6fa
Polyane Tuag <pollyportugues@hotmail.com>
2021-06-04 19:18:10 -0300

    Title dinâmico por página

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
+import Head from 'next/head'
+
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello world
+    <>
+      <Head>
+          <title>ig.News</title>
+        </Head>
+
+      <h1 className={styles.title}>
+        Hello world
       </h1>
+    </>
   )
 }
