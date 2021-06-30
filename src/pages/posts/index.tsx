77e9ce583657c23cb18cded98704ff26e59e93ae
Polyane Tuag <pollyportugues@hotmail.com>
2021-06-30 18:59:01 -0300

    formatando dados dos posts listados

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,8 +3,20 @@ import Head from 'next/head';
 import { GetStaticProps } from 'next';
 import { getPrismicClient } from '../../services/prismic';
 import Prismic from '@prismicio/client';
+import {RichText} from 'prismic-dom';
 
-export default function Posts() {
+type Post = {
+  slug: string,
+  title: string,
+  excerpt: string,
+  updatedAt: string,
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -13,23 +25,14 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="">
-            <time> 28 de junho de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared button</p>
+          { posts.map(post => (
+          <a key= {post.slug} href="">
+            <time> {post.updatedAt}</time>
+            <strong>{post.title}</strong>
+            <p>{post.excerpt}</p>
           </a>
+          ))}
 
-          <a href="">
-            <time> 28 de junho de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared button</p>
-          </a>
-
-          <a href="">
-            <time> 28 de junho de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared button</p>
-          </a>
         </div>
       </main>
     </>
@@ -46,9 +49,22 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100
   })
 
-  // console.log(JSON.stringify(response, null, 2))
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    };
+  });
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
