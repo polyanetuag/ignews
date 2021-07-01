8a2556d56e4e096ecb1c3dba904a87c0d9da3b71
Polyane Tuag <pollyportugues@hotmail.com>
2021-07-01 18:36:14 -0300

    Add navegação no menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
 import { SignInButton } from '../SignInButton';
+import Link from 'next/link'
+
 import styles from './styles.module.scss';
 
 export function Header() {
@@ -7,8 +9,13 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="Logo" />
         <nav>
+          <Link href="/">
           <a className={styles.active}>Home</a>
+          </Link>
+
+          <Link href="/posts">
           <a >Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
