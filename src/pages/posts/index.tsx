c2838e854e152560e5b769165c2bad8cab83463c
Polyane Tuag <pollyportugues@hotmail.com>
2021-07-02 21:19:03 -0300

    Criando a página dos posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -4,6 +4,7 @@ import { GetStaticProps } from 'next';
 import { getPrismicClient } from '../../services/prismic';
 import Prismic from '@prismicio/client';
 import {RichText} from 'prismic-dom';
+import Link from 'next/link';
 
 type Post = {
   slug: string,
@@ -26,11 +27,13 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           { posts.map(post => (
-          <a key= {post.slug} href="">
-            <time> {post.updatedAt}</time>
-            <strong>{post.title}</strong>
-            <p>{post.excerpt}</p>
-          </a>
+            <Link href={`/posts/${post.slug}`}>
+              <a key= {post.slug} >
+                <time> {post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+          </Link>
           ))}
 
         </div>
